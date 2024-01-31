3a81172a4cbb0397e46610a60d885b135917e4ae
hotsno <71658949+hotsno@users.noreply.github.com>
2024-01-31 11:45:11 -0800

    Update user.js

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,12 +1,12 @@
+
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
 user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
 user_pref("privacy.history.custom", true);
-user_pref("browser.bookmarks.restore_default_bookmarks", false);
-user_pref("browser.bookmarks.file", "");
 user_pref("extensions.pocket.enabled", false);
 user_pref("extensions.screenshots.disabled", true);
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("ui.key.menuAccessKeyFocuses", false);
+user_pref("browser.tabs.firefox-view", false);
