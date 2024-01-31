f479bcfdac125e91543ba07afe9d3538beba08a1
hotsno <71658949+hotsno@users.noreply.github.com>
2024-01-31 08:00:24 -0800

    Add prefs.js

---
+++ .config/firefox/prefs.js
@@ -0,0 +1,8 @@
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("privacy.history.custom", true);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+user_pref("browser.bookmarks.file", "");
